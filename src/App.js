5dbbfeca79227e0d3402c6b35e018639e9c2c84c
Sahoo <artbangla21@gmail.com>
2023-12-06 00:39:09 +0530

    this is first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+
+  const [count, setCount] = useState("")
+
+  useEffect(() => {
+   console.log('Effect runs');
+  
+  }, []);
+  const handleOnclick = ()=> {
+
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" />
+        <button>Search</button>
+      </form>
+      {count}
+      <button onclick={handleOnclick}>Click</button>
     </div>
   );
 }
